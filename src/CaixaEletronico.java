8159fc85b105010e6b7ef0cc0b73c8760ce3577a
marcoshsena <marcoshsena.jr@gmail.com>
2024-05-10 11:15:00 -0300

    Todas as Condicionais

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,5 +1,7 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
+       
+        // Condicionais Simple e Compostas
         double saldo = 25.0;
         double valorSolicitado = 22.0;
 
@@ -11,15 +13,35 @@ public class CaixaEletronico {
             System.out.println("Saldo Insuficiene");
         
 
-
-        int nota = 8;
+        // Condinais Encadeadas
+        int nota = 6;
        
        if(nota >= 7)
         System.out.println("Aprovado");
 
-       else
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de Recuperação");
+
+        else
         System.out.println("Reprovado");
 
+        // Condição Ternária
+        String resultado = nota >= 7 ? "Aprovado" : nota >+5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
 
+        // Switch Case
+        String plano = "T"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
     }
 }
